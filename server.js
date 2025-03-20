c237b9095b36536b53e32f604b96c201b939109c
Algorythm <124550715+pHpAlgoRythm@users.noreply.github.com>
2025-03-20 23:12:14 +0800

    modify

--- server.js
+++ server.js
@@ -1,39 +1,52 @@
-
-
 const express = require('express');
 const http = require('http');
-const {Server} = require('socket.io');
-const cors = require('cors')
+const { Server } = require('socket.io');
+const cors = require('cors');
+const { Socket } = require('dgram');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server,{
+const io = new Server(server, {
     cors: {
-        origin: '*',
-        method:['GET', 'POST']
+        origin : '*',
+        methods: ['GET', 'POST']
     }
 });
 
 app.use(cors());
+app.use(express.json());
 
-io.on('connection', (socket) => {
-    console.log('A User connected', socket.id);
+let clients = [];
+
+io.on('connection', (socket)=> {
+    console.log('A user Connected:', socket.id);
+    clients.push(socket);
 
     socket.on('disconnect', () => {
         console.log('User disconnected', socket.id);
-
-       
+        clients = clients.filter(client => client !== socket);
     });
 });
 
-app.post("/broadcast", express.json(), (req, res)=> {
-    const {user} = req.body;
-    io.emit('pendingUser', user);
-    res.status(200).json({message: "User broadcastes"});
+app.post('/broadcast', (req, res) => {
+    try{
+        const {user} = req.body;
+        if(!user){
+            return res.status(400).json({message: 'user data missing'});
+
+        }
+        console.log("Broadcasting new user:", user);
+
+        // Emit event to all connected clients
+        io.emit('PendingUser', user);
+
+        res.status(200).json({ message: "User broadcasted" });
+    } catch (error) {
+        res.status(500).json({ message: "Internal Server Error", error: error.message });
+    }
 });
 
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, ()=>{
-    console.log(`webSocket server running on port ${PORT}`);
+const PORT =  3000;
+server.listen(PORT, () => {
+    console.log(`WebSocket server running on port ${PORT}`);
 });
