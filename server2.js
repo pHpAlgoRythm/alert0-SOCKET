65fb56ca649546b9edccc95c0c31f8fd0f0f74c8
Algorythm <124550715+pHpAlgoRythm@users.noreply.github.com>
2025-03-22 10:49:49 +0800

    add react websocket

---
+++ server2.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+// Enable CORS for HTTP requests
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  })
+);
+
+// Create a Socket.IO server with CORS enabled
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on('send', (message) => {
+    io.emit('receive', message);
+  });
+
+  socket.on('register', (data) => {
+    io.emit('PendingUser');
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
